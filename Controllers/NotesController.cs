3e7aef8846fcd21845562a7176dec41bb0b3dd6d
cleu892 <128200238+kaixilee@users.noreply.github.com>
2024-06-19 19:41:07 +1200

    setup some more stuff

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,36 @@
+﻿// Controllers/NotesController.cs
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DigitalDiary.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotesController : ControllerBase
+    {
+        private static List<string> notes = new List<string>();
+
+        [HttpGet]
+        public IActionResult GetNotes()
+        {
+            return Ok(notes);
+        }
+
+        [HttpPost]
+        public IActionResult AddNote([FromBody] Note note)
+        {
+            if (note == null || string.IsNullOrWhiteSpace(note.Content))
+            {
+                return BadRequest(new { message = "Note content cannot be empty" });
+            }
+
+            notes.Add(note.Content);
+            return Ok(new { note = note.Content });
+        }
+    }
+
+    public class Note
+    {
+        public string Content { get; set; }
+    }
+}
